5f4162bccc4eb48008f6cfa6076aa47475adb0ca
railstone <mark26acc@gmail.com>
2011-02-26 13:46:51 +0100

    Initial commit

---
+++ rails_projects/second_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '565d0616f7305c2f0239bc7973a59ef52585a860e4ec7f86f36eb2b352d7a069623cee66e24ec905de8fa2b6e99900a8ab19f089943faa4e3268e2e1532fe635'
